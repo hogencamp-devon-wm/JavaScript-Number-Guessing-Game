1c71722adaf3f544af84f20bd0981108e2649146
Devon Hogencamp <devon.hogencamp@west-mec.org>
2016-08-11 15:26:49 -0700

    Not done yet

---
+++ script.js
@@ -0,0 +1,63 @@
+// Ask user if they want to play the game
+var game = confirm("Do you want to play the number guessing game?");
+
+// If they want to play
+while (game == true) {
+    // Generate Random number
+    var randomNumber = Math.random();
+
+    // Make it number bettween 1 and 100
+    randomNumber = randomNumber * 100;
+
+    // Rounds the number to the nearest whole number
+    randomNumber = Math.floor(randomNumber);
+
+    // Ask the user what they think the number is
+    var userGuess = prompt("Guess a number bettween 1-100");
+
+    // Track number of guesses
+    var guesses = 1;
+
+    while (userGuess != randomNumber) {
+        // Check to see if user guess is a number
+        if (isNaN(userGuess)) {
+            // If its not a number
+            alert("Please enter a number")
+        }
+
+        else {
+            // They put a number in
+            if (1 < userGuess < 100) {
+                // Correct Range
+
+                if (userGuess < randomNumber) {
+                    // If number is to low
+                    userGuess = prompt("Your to low\n\nGuess a number bettween 1-100");
+                    guesses++;
+                }
+                else if (userGuess > randomNumber) {
+                    // If number is to high
+                    userGuess = prompt("Your to high\n\nGuess a number bettween 1-100");
+                    guesses++;
+                }
+                else {
+                    // If they got the number right
+                    guesses++;
+                    game = confirm("You got it right the number was " + randomNumber + " it took you " + guesses + " guesses. Do you want to play again?");
+                }
+            }
+
+            else {
+                // Wrong Range
+                alert("Enter a number bettween 1-100");
+            }
+        }
+    }
+
+
+}
+
+// If they dont want to play
+if (game == false) {
+    alert("K, bye:(");
+}
